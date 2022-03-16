9725b32436e95e34c55d7defeb150f3390ade45e
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-03-15 20:58:27 -0400

    Timer 2 Beeper Function

---
+++ timer2.js
@@ -0,0 +1,12 @@
+let args = process.argv;
+args = args.slice(2);
+
+const timer = num => {
+    for (let i = 0; i <= num; i ++) {
+        setTimeout(() => {
+          process.stdout.write('\x07');
+        }, num[i] * 1000);
+    }
+};
+
+timer(args);
