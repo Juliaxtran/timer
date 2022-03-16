afd35b0cd7a6c1a6acbe0c97881999713817ab2c
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-03-16 08:34:15 -0400

    FV

--- timer2.js
+++ timer2.js
@@ -2,7 +2,7 @@ let args = process.argv;
 args = args.slice(2);
 
 const timer = num => {
-    for (let i = 0; i <= num; i ++) {
+    for (let i = 0; i  < num.length; i ++) {
         setTimeout(() => {
           process.stdout.write('\x07');
         }, num[i] * 1000);
